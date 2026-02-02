75ddad596f5bedf5f09a9bbb5ced12accd82b65a
REME <asreme@qq.com>
2026-02-02 16:08:44 +0800

    2026-02-02 even more tips

--- Tutorials/04 - 本地化/Strike.java
+++ Tutorials/04 - 本地化/Strike.java
@@ -4,7 +4,7 @@ import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import basemod.abstracts.CustomCard;
 
 public class Strike extends CustomCard {
-    public static final String ID = ModHelper.makePath("Strike");
+    public static final String ID = ModHelper.makePath(Strike.class.getSimpleName());    // ID（此处的ModHelper在“04 - 本地化”的进阶中提到）
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID); // 从游戏系统读取本地化资源
     // private static final String NAME = "打击";
     private static final String NAME = CARD_STRINGS.NAME; // 读取本地化的名字
